03ba739227b24c1a19d7dd2c8f2eb0dfcfb53bcf
Stephanie Leung <sleung@ie.ibm.com>
2020-10-02 12:10:40 +0100

    updates required packages

--- FBI_Incidents_Analysis/setup.bat
+++ FBI_Incidents_Analysis/setup.bat
@@ -1,5 +1,6 @@
 REM First run to install requirement
 pip3 install -r requirements.txt
+python3 -m ipykernel install --user
 
 REM Go to the project directory and run main.py
 cd Data_Source
--- FBI_Incidents_Analysis/setup.sh
+++ FBI_Incidents_Analysis/setup.sh
@@ -1,4 +1,5 @@
 pip3 install -r requirements.txt
+python3 -m ipykernel install --user
 
 cd Data_Source
 python main.py
